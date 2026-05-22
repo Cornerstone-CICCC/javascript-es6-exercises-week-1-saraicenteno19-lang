e3bc1ea771c9ce91460afae02936bf9c440f6aa8
Sarai <sarai@MacBook-Neo-de-Sarai.local>
2026-05-22 12:55:34 -0400

    Exercise 18 completed

--- exercises/ex18.js
+++ exercises/ex18.js
@@ -1,6 +1,5 @@
-/*
-Write a named function expression that stores the function in a variable called cry and returns "boohoo!". Don't forget to call the function using the variable name, not the function name:
+var cry = function boohoo() {
+    return "boohoo!";
+};
 
-cry();
-Returns: boohoo!
-*/
+console.log(cry());
