9f6eb1f937990839398dc1fe6a86865b96527d34
Sarai <sarai@macbookodesarai.home>
2026-05-25 15:36:01 -0400

    Exercise 28 completed

--- exercises/ex28.js
+++ exercises/ex28.js
@@ -1,13 +1,34 @@
-/*
-Create an object called facebookProfile. The object should have 3 properties:
+const facebookProfile = {
+  name: "Sarai",
+  friends: 0,
+  messages: [],
 
-your name
-the number of friends you have, and
-an array of messages you've posted (as strings)
-The object should also have 4 methods:
+  postMessage: function (message) {
+    this.messages.push(message);
+  },
 
-postMessage(message) - adds a new message string to the array
-deleteMessage(index) - removes the message corresponding to the index provided
-addFriend() - increases the friend count by 1
-removeFriend() - decreases the friend count by 1
-*/
+  deleteMessage: function (index) {
+    this.messages.splice(index, 1);
+  },
+
+  addFriend: function () {
+    this.friends += 1;
+  },
+
+  removeFriend: function () {
+    if (this.friends > 0) {
+      this.friends -= 1;
+    }
+  }
+};
+facebookProfile.addFriend();
+facebookProfile.addFriend();
+facebookProfile.postMessage("Hola mundo");
+facebookProfile.postMessage("Segundo mensaje");
+
+console.log(facebookProfile);
+
+facebookProfile.deleteMessage(0);
+facebookProfile.removeFriend();
+
+console.log(facebookProfile);
